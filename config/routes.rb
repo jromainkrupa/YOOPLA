4db5eb5e630dbe3d6b0c892eaa1d9becaa63e538
jeanromain KRUPA <jkrupa@hotmail.fr>
2020-10-27 11:13:54 +0100

    thank after creation done

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "pages#home"
+  get '/thanks', to: "users#thanks"
   # get "users/new", to: "users#new"
-  resources :users, only: [:new, :create]
+  resources :users, only: [:new, :create, :show]
   post "users", to: "users#create"
 end
