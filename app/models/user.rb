22dc9f965a8d40067397fa4f38f027834076f178
jeanromain KRUPA <jkrupa@hotmail.fr>
2020-10-28 16:42:20 +0100

    email sending after create

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,11 @@
 class User < ApplicationRecord
-    validates :email, presence: true, uniqueness: true, format: { with: /[^@\s]+@[^@\s\.]+\.[^@\.\s]+/,
-    message: "Invalid email" }
+  validates :email, presence: true, uniqueness: true, format: { with: /[^@\s]+@[^@\s\.]+\.[^@\.\s]+/,
+  message: "Invalid email" }
+  after_create :send_welcome_email
+
+  private
+
+  def send_welcome_email
+    UserMailer.with(user: self).welcome.deliver_now
+  end
 end
