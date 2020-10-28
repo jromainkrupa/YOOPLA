68723f16d63501e7c9e6a1499900aa8b220b6135
jeanromain KRUPA <jkrupa@hotmail.fr>
2020-10-28 15:16:55 +0100

    set up postmark

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,7 +5,7 @@ Rails.application.configure do
   config.cache_classes = true
 
   #mailer
-    config.action_mailer.delivery_method     = :postmark
+  config.action_mailer.delivery_method     = :postmark
   config.action_mailer.postmark_settings   = { api_token: ENV['POSTMARK_API_TOKEN'] }
   config.action_mailer.default_url_options = { host: "www.yoopla.io" }
 
