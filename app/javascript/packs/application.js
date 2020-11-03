29913bbec321b220648cdd2f10737a24f2b63ee9
jeanromain KRUPA <jkrupa@hotmail.fr>
2020-11-03 14:06:51 +0100

    menu toggler js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,9 +9,17 @@ require("@rails/activestorage").start()
 require("channels")
 
 import "stylesheets/application"
+import "../components/mobile_navbar_toggler.js"
+import { mobileNavbarToggler } from "../components/mobile_navbar_toggler.js"
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+document.addEventListener('turbolinks:load', () => {
+  // Call your functions here, e.g:
+  // initSelect2();
+  mobileNavbarToggler();
+});
