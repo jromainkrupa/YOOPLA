1f433e052cc00f20432585430ccb1884288a80de
jeanromain KRUPA <jkrupa@hotmail.fr>
2020-11-15 16:52:00 +0100

    deleted pricing page

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'pages#home'
   get '/method', to: 'pages#method'
-  get '/pricing', to: 'pages#pricing'
-  get '/company', to: 'pages#company'
+  #get '/pricing', to: 'pages#pricing'
+  #get '/company', to: 'pages#company'
   get '/features', to: 'pages#features'
   # get 'users/new', to: 'users#new'
   resources :users, only: [:new, :create, :show]
