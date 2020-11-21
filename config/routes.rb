3e7aa9f9b4a8e3972f41621f969e760663222e3b
jeanromain KRUPA <jkrupa@hotmail.fr>
2020-11-21 13:53:48 +0100

    setup i18n done

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users, only: [:show]
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: 'pages#home'
-  get '/method', to: 'pages#method'
-  #get '/pricing', to: 'pages#pricing'
-  #get '/company', to: 'pages#company'
-  get '/features', to: 'pages#features'
-  # get 'users/new', to: 'users#new'
-  resources :users, only: [:new, :create, :show]
-  post 'users', to: 'users#create'
+
+  scope '(:locale)', locale: /fr/ do
+    root to: 'pages#home'
+    get '/method', to: 'pages#method'
+    #get '/pricing', to: 'pages#pricing'
+    #get '/company', to: 'pages#company'
+    get '/features', to: 'pages#features'
+    # get 'users/new', to: 'users#new'
+    resources :users, only: [:new, :create, :show]
+    post 'users', to: 'users#create'
+  end
 end
